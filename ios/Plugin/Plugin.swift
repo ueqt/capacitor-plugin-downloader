5375412af7f71e725f971fa555563353457a6baa
ueqt <ueqtxu@gmail.com>
2023-06-13 14:59:31 +0800

    fix

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -40,11 +40,16 @@ public class DownloaderPlugin: CAPPlugin {
                 }
             }
             .responseData { response in
+                print("finish download")
                 if response.error == nil {
                     // unzip
+                    print("no error")
                     let _ = self.unzip(fileURL)
+                    print("finish unzip")
+                    print(fileURL.absolutePath)
                     call.resolve()
                 } else {
+                    print("error")
                     call.reject(response.error?.errorDescription ?? "")
                 }
             }
@@ -64,16 +69,22 @@ public class DownloaderPlugin: CAPPlugin {
     }
     
     private func unzip(_ fileURL: URL) -> String {
-        if fileURL.pathExtension == ".zip" {
+        print(fileURL.pathExtension)
+        if fileURL.pathExtension == "zip" {
+            print("zip")
             SSZipArchive.unzipFile(atPath: fileURL.absoluteString, toDestination: (fileURL.absoluteString as NSString).deletingPathExtension)
+            print("success unzip")
             // delete file
             do {
+                print("start delete")
                 try FileManager.default.removeItem(at: fileURL)
+                print("finish delete")
             } catch {
                 print("Could not delete file, probably read-only filesystem")
             }
             return (fileURL.absoluteString as NSString).deletingPathExtension.appending("/index.html")
         }
+        print("not zip")
         return fileURL.absoluteString
     }
 }
